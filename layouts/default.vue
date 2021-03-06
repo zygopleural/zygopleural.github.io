1dcdde2e7becb5d07e906f5c3874414713551af1
Lee Staples <lmstaples94@gmail.com>
2021-03-06 14:48:18 +0000

    Updates homepage

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,9 @@
   <v-app dark>
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
+      clipped
+      expand-on-hover
+      permanent
       app
     >
       <v-list>
@@ -24,21 +24,16 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
+    <v-app-bar clipped-left app elevate-on-scroll>
+      <v-app-bar-nav-icon>
+        <v-avatar size="36px">
+          <img alt="Avatar" src="/lee.jpeg" />
+        </v-avatar>
+      </v-app-bar-nav-icon>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
+      <v-btn icon @click.stop="">
+        <v-icon>mdi-application</v-icon>
       </v-btn>
     </v-app-bar>
     <v-main>
@@ -46,17 +41,7 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
+    <v-footer absolute app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -66,25 +51,25 @@
 export default {
   data() {
     return {
-      clipped: false,
       drawer: false,
-      fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-home',
+          title: 'Home',
           to: '/',
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
+          icon: 'mdi-laptop',
+          title: 'Experience',
+          to: '/experience',
+        },
+        {
+          icon: 'mdi-school',
+          title: 'Education',
+          to: '/education',
         },
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+      title: 'Hey, I’m Lee 👋🏼',
     }
   },
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - zygopleural',
-    title: 'zygopleural',
+    titleTemplate: '%s - Lee Staples',
+    title: 'Home',
     htmlAttrs: {
       lang: 'en',
     },
